52452ef8f034e793c233ea090442923bb2585c70
DanielVII <danieldantaswa@gmail.com>
2022-03-28 13:12:34 -0300

    adicionando habilidades

--- src/card/Card.js
+++ src/card/Card.js
@@ -5,11 +5,34 @@ function Card() {
     <div class="card-posicao">
       <img src="https://i.imgur.com/1h6AovY.jpeg" 
       alt="imagem da carta" class="card-em-si"/>
-      <button type="button"  class="habilidades">
-      <img src="https://i.imgur.com/IcXE6ap.jpeg" alt="doguinho"  id="passiva"/>
+      
+
+      <button type="button"  class="botao-habilidade" id="botao-passiva" onClick="acao-passiva">
+      <img src="https://i.imgur.com/IcXE6ap.jpeg" alt="img-habilidade" class="img-habilidade" id="img-passiva"/>
       </button>
+
+      <button type="button"  class="botao-habilidade" id="botao-habilidade-arma">
+      <img src="https://i.imgur.com/IcXE6ap.jpeg" alt="img-habilidade" class="img-habilidade" id="img-habilidade-arma"/>
+      </button>
+
+      <button type="button"  class="botao-habilidade" id="botao-habilidade-um">
+      <img src="https://i.imgur.com/IcXE6ap.jpeg" alt="img-habilidade" class="img-habilidade" id="img-habilidade-um"/>
+      </button>
+
+      <button type="button"  class="botao-habilidade" id="botao-habilidade-dois">
+      <img src="https://i.imgur.com/IcXE6ap.jpeg" alt="img-habilidade" class="img-habilidade" id="img-habilidade-dois"/>
+      </button>
+
+      {/* <script>
+        function acao-passiva() {
+          const passiva = document.querrySelect("#img-passiva");
+
+        }
+      </script>  */}
+
     </div>
       
+    
   );
 }
 
